85c265e13cef2a063441fd9de4d579ba75567cce
ConCEAT <diegolares@wp.pl>
2020-08-04 00:30:26 +0200

    Refactor: rename and reorganise logic into classes

---
+++ logic.py
@@ -0,0 +1,49 @@
+import os
+import json
+import requests
+import urllib.request
+from datetime import datetime
+
+from typing import Dict
+
+class Comic:
+    @staticmethod
+    def local_images(path: str) -> list:
+        "Return ids of all images in a directory"
+        for (_, _, filenames) in os.walk(path):
+            if filenames:
+                return list(map(lambda filename: int(filename.split('.')[0]), filenames))
+            return []
+        raise ValueError("Incorrect path given")
+
+    @staticmethod
+    def save_image(path: str, filename: str, url: str) -> None:
+        """Save image from URL to /path/filename.ext
+
+        File extension is taken from URL
+        """
+        file_extension = url.split('.')[-1]
+        image = requests.get(url).content
+        with open(os.path.join(path,f'{filename}.{file_extension}'), 'wb') as new_image:
+            new_image.write(image)
+
+    @staticmethod
+    def load_json(url: str) -> dict:
+        "Load JSON file from URL to python dictionary"
+        with urllib.request.urlopen(url) as source:
+            text = source.read().decode('utf8')
+        return json.loads(text)
+
+    @staticmethod
+    def process_data(comic_data: Dict) -> dict:
+        "Format comic data into n"
+        return {
+            "id" : comic_data.get("num"),
+            "description" : comic_data.get("transcript"),
+            "date" : datetime(
+                int(comic_data.get("year")),
+                int(comic_data.get("month")),
+                int(comic_data.get("day"))).strftime("%y-%m-%d"),
+            "title" : comic_data.get("title").lower(),
+            "url" : comic_data.get("img")
+        }
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 from typing import Optional, Dict, List
 from fastapi import FastAPI, Response, Query
 
-import functions
+from logic import Comic
 
 load_dotenv()
 app = FastAPI()
@@ -19,11 +19,11 @@ HOST = os.getenv('HOST')
 @app.get("/comics/current")
 def get_current_comic(response: Response):
     try:
-        comic_data = functions.load_json(f'{HOST}/info.0.json')
+        comic_data = Comic.load_json(f'{HOST}/info.0.json')
     except urllib.error.HTTPError as error:
         response.status_code = error.code
         return error
-    return functions.process_comic_data(comic_data)
+    return Comic.process_data(comic_data)
 
 
 @app.get("/comics/many")
@@ -32,11 +32,11 @@ def get_many_comics(response: Response, comic_ids: List[int] = Query(...)):
     unique_ids = list(dict.fromkeys(comic_ids))
     for comic_id in unique_ids:
         try:
-            comic_data = functions.load_json(f'{HOST}/{comic_id}/info.0.json')
+            comic_data = Comic.load_json(f'{HOST}/{comic_id}/info.0.json')
         except urllib.error.HTTPError as error:
             response.status_code = error.code
             return error
-        output.append(functions.process_comic_data(comic_data))
+        output.append(Comic.process_data(comic_data))
     return output
 
 
@@ -44,27 +44,27 @@ def get_many_comics(response: Response, comic_ids: List[int] = Query(...)):
 def download_comics(response: Response, comic_ids: List[int] = Query(...)):
     urls = []
     images_path = os.getenv('IMAGES_PATH')
-    local_files = dict.fromkeys(functions.get_filenames(images_path), True)
-    unique_ids = [ID for ID in dict.fromkeys(comic_ids) if not local_files.get(ID)]
+    local_images = dict.fromkeys(Comic.local_images(images_path), True)
+    unique_ids = [ID for ID in dict.fromkeys(comic_ids) if not local_images.get(ID)]
 
     for comic_id in unique_ids:
         try:
-            comic_data = functions.load_json(f'{HOST}/{comic_id}/info.0.json')
+            comic_data = Comic.load_json(f'{HOST}/{comic_id}/info.0.json')
         except urllib.error.HTTPError as error:
             response.status_code = error.code
             return error
-        urls.append((comic_id,comic_data['img']))
+        urls.append((comic_id,comic_data.get('img')))
     
     for comic_id, image_url in urls:
-        functions.save_image(images_path, str(comic_id), image_url)
+        Comic.save_image(images_path, str(comic_id), image_url)
     return {}
 
 
 @app.get("/comics/{comic_id}")
 def get_comic_by_id(comic_id: int, response: Response):
     try:
-        comic_data = functions.load_json(f'{HOST}/{comic_id}/info.0.json')
+        comic_data = Comic.load_json(f'{HOST}/{comic_id}/info.0.json')
     except urllib.error.HTTPError as error:
         response.status_code = error.code
         return error
-    return functions.process_comic_data(comic_data)
+    return Comic.process_data(comic_data)
